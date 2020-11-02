1feeade3c85e29d566c3296037db17b135423ef0
Liz Burton <liz.burton147@gmail.com>
2020-11-02 15:08:17 -0500

    fixes starter code

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -12,13 +12,6 @@ class GetPrograms
     response.body
   end
 
-  def program_school
-    programs = JSON.parse(self.get_programs)
-    programs.collect do |program|
-      program["agency"]
-    end
-  end
-
 end
 
 programs = GetPrograms.new.get_programs
