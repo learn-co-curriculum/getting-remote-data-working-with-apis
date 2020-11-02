932927da0203275f542b8440d09cca4862c085de
Liz Burton <liz.burton147@gmail.com>
2020-11-02 15:04:12 -0500

    fixes indenting

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,8 +1,8 @@
 require 'net/http'
- require 'open-uri'
- require 'json'
+require 'open-uri'
+require 'json'
  
- class GetPrograms
+class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
@@ -21,5 +21,5 @@ require 'net/http'
 
 end
 
- programs = GetPrograms.new.get_programs
- puts programs
+programs = GetPrograms.new.get_programs
+puts programs
